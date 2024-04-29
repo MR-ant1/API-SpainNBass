e10a3477550dabcd084057f82f4c039118d44c03
antonio <antoniorc7@gmail.com>
2024-04-29 20:47:40 +0200

    funcionalidad: conexión a base de datos y backend creada. Pendiente arreglar bug instalaciones

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 
-import express, { Application } from "express";
-
-import cors from "cors"
-
+import express, { Application } from 'express';
+import cors from 'cors'
 
 export const app: Application = express();
 
-
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+
+app.get('/healthy', (req, res) => {
+    res.status(200).json(
+        {
+            success: true,
+            message: 'Server is healthy'
+        }
+    );
+})
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,21 @@
 
-// import 'dotenv/config';
-// import { app } from './app';
-// import 
+import 'dotenv/config';
+import { app } from './app';
+import { AppDataSource } from './database/db';
+
+const PORT = process.env.PORT || 4002;
+
+const startServer = () => {
+    AppDataSource.initialize()
+        .then(() => {
+            console.log('database connected')
+
+            app.listen(PORT, () => {
+                console.log(`server is running on port: ${PORT}`)
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+startServer()
